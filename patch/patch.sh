5b9872590c99ee1e3e9b762be3a2bf1d1a86d84b
Sander <sander.schutten@macaw.nl>
2017-05-09 00:32:53 +0200

    Add files via upload

--- patch/patch.sh
+++ patch/patch.sh
@@ -1,1 +1,14 @@
+ #!/bin/bash 
 
+echo Setting defconfig 
+cd ../snx_sdk/buildscript/
+make sn98660_QR_Scan_402mhz_sf_defconfig
+
+echo Applying patch
+cd ../../patch/
+/bin/cp -f -R ./snx_sdk/* ../snx_sdk/
+
+echo Executing make
+cd ../snx_sdk/buildscript/
+#make
+#make install
