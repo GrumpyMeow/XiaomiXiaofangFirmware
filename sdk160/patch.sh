5d5b3db5de920f93c131d57ca24e6cc4b5b38a42
root <root@localhost.localdomain>
2017-05-17 16:13:55 -0400

    test

--- sdk160/patch.sh
+++ sdk160/patch.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
-echo Setting defconfig
+echo -e "Setting defconfig"
 cd ../../snx_sdk/buildscript/
 
 make sn98660_QR_Scan_402mhz_sf_defconfig
 
-echo Applying patch
+echo -e "Applying patch"
 cd ../../sdk160/patch/
 /bin/cp -v -f -R snx_sdk/* ../../snx_sdk/
 
-echo Executing make
-cd ../snx_sdk/buildscript/
+echo -e "Executing make"
+cd ../../snx_sdk/buildscript/
 
 make
 
-echo Executing make install
+echo -e "Executing make install"
 make install
 
