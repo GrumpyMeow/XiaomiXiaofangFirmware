a3acc3961bd5762ffa1b32ffc6beecca71d69bf5
Sander <sander.schutten@macaw.nl>
2017-05-17 15:45:30 -0400

    Update patch.sh

--- sdk160/patch.sh
+++ sdk160/patch.sh
@@ -1,82 +1,19 @@
-#!/bin/bash 
+#!/bin/bash
 
-echo -e "** Xiaomi Xiaofang Custom firmware builder script **\n"
+echo Setting defconfig
+cd ../../snx_sdk/buildscript/
 
-echo "Clean SDK filesystem"
-/bin/rm -f -r ../snx_sdk/filesystem/rootfs/src/target/*
-/bin/rm -f -r ../snx_sdk/filesystem/rootfs-rescue/src/target/*
-echo -e " Done\n"
+make sn98660_QR_Scan_402mhz_sf_defconfig
 
-echo -e "Extracting firmware rootfs files"
-mkdir /mnt/rootfs
-mount  -o loop -t cramfs ./3.0.3.56/rootfs.cramfs /mnt/rootfs/ 
-(cd /mnt/rootfs/ ; tar cfp - .)|(cd ../snx_sdk/filesystem/rootfs/src/target/ ; tar xfp  - --warning=no-timestamp)
-(cd /mnt/rootfs/ ; tar cfp - .)|(cd ../snx_sdk/filesystem/rootfs-rescue/src/target/ ; tar xfp - --warning=no-timestamp)
-umount /mnt/rootfs
-rmdir /mnt/rootfs
-/bin/rm -f ../snx_sdk/filesystem/rootfs/src/target/lib/modules/2.6.35.12/kernel/drivers/ar0130.ko
-/bin/rm -f ../snx_sdk/filesystem/rootfs/src/target/lib/modules/2.6.35.12/kernel/drivers/imx322.ko
-/bin/rm -f ../snx_sdk/filesystem/rootfs/src/target/lib/modules/2.6.35.12/kernel/drivers/ov9715.ko
-/bin/rm -f ../snx_sdk/filesystem/rootfs/src/target/lib/modules/2.6.35.12/kernel/drivers/sc2035.ko
+echo Applying patch
+cd ../../sdk160/patch/
+/bin/cp -v -f -R snx_sdk/* ../../snx_sdk/
 
-echo -e " Done\n"
-
-echo "Patching SDK files"
-cd snx_sdk
-/bin/cp  -f -r . ../../snx_sdk/
-cd ..
-echo -e " Done\n"
-
-echo "Providing nvram.bin"
-/bin/cp -f ./needed/nvram.bin ../snx_sdk/image/
-echo -e " Done\n"
-
+echo Executing make
 cd ../snx_sdk/buildscript/
-make firmware
-cd ../../patch/
 
-/bin/rm -f -r ../snx_sdk/sdcard/
-mkdir ../snx_sdk/sdcard/
+make
 
-/bin/cp -r -f ../snx_sdk/app/web-admin/* ../snx_sdk/sdcard/rootfs/
-/bin/cp -r -f ../snx_sdk/app/galaxy-server/* ../snx_sdk/sdcard/rootfs/
-/bin/cp -r -f ../snx_sdk/app/example/usr/* ../snx_sdk/sdcard/rootfs/usr
-
-
-
-
-
-find ../rootfs/ -type f | xargs ../toolchain/crosstool-4.5.2/arm-linux/bin/strip -s
-find ../rootfs-rescue/ -type f | xargs ../toolchain/crosstool-4.5.2/arm-linux/bin/strip -s
-
-#make rootfs
+echo Executing make install
 make install
 
-cd  ../../patch/
-
-read -p "Press any key to 'make the firmware'... " -n1 -s
-echo -e "\n"
-
-echo "Copying KERNEL.bin from source firmware"
-
-/bin/cp -f ./3.0.3.56/KERNEL.bin ../snx_sdk/image/
-echo -e "\tDone"
-
-echo "Creating uImage file from KERNEL.bin"
-dd if=../snx_sdk/image/KERNEL.bin of=../snx_sdk/image/uImage bs=1 skip=4 count=$(( $(stat -c %s ../snx_sdk/image/KERNEL.bin) - 116 )) iflag=skip_bytes,count_bytes
-echo -e "\tDone\n"
-
-echo "Duplicating uImage to rescue_uImage (for now)"
-/bin/cp -f ../snx_sdk/image/uImage ../snx_sdk/image/rescue_uImage
-echo -e "\tDone\n"
-
-echo "Making firmware" 
-cd ../snx_sdk/buildscript/
-make kernelimage
-make firmware
-
-echo "Copying firmware file (FIRMWARE_660R.bin) to PATCH directory"
-/bin/cp -f ../image/FIRMWARE_660R.bin ../../patch/
-echo -e "\tDone\n"
-echo "Place the FIRMWARE_660R.bin file in the root of a SDCard"
-echo "Hold the setup button during boot"
