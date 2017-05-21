e4dcf11f4def632abf36f3478042dd069b6003a1
root <root@localhost.localdomain>
2017-05-21 01:36:35 -0400

    test

--- sdk160/patch.sh
+++ sdk160/patch.sh
@@ -5,14 +5,11 @@ cd ../../snx_sdk/buildscript/
 
 make clean
 
-make sn98660_402mhz_sf_defconfig
+#make sn98660_402mhz_sf_defconfig
+make sn98660_QR_Scan_402mhz_sf_defconfig
 
 echo -e "Applying patch"
 cd ../../XiaomiXiaofangFirmware/sdk160/
 /bin/cp -v -f -R snx_sdk/* ../../snx_sdk/
 
-echo -e "execute: make menuconfig kernelmenuconfig make make install"
-
-#in kernelmenuconfig: System Type not correctly visible in 
-#mapping drivers for chip access
-#support non lineair , mtd_physmap_len uitzoeken
+echo -e "execute: make menuconfig, make kernelmenuconfig, make, make install, make firmware"
