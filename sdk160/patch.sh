acec9061c97786899a238aba4227ece48fd8a8db
root <root@localhost.localdomain>
2017-05-24 00:44:15 -0400

    test

--- sdk160/patch.sh
+++ sdk160/patch.sh
@@ -13,19 +13,19 @@ echo -e "Setting defconfig"
 cd ../../snx_sdk/buildscript/
 make sn98660_QR_Scan_402mhz_sf_defconfig
 
+echo -e "execute: cd ../../snx_sdk/buildscript/"
 echo -e "execute: make menuconfig"
 
 echo -e "execute: make kernelmenuconfig"
-echo -e "\tselect: 'General setup > Kernel compression mode > LZMA'"
-echo -e "\tselect: 'General setup > Optimize for size'"
-echo -e "\tselect: 'System Type > SONiX SN986XX Implementations >  Select target board > Support SONiX SN98660 Platform'"
+#echo -e "\tselect: 'General setup > Kernel compression mode > LZMA'"
+#echo -e "\tselect: 'General setup > Optimize for size'"
+#echo -e "\tselect: 'System Type > SONiX SN986XX Implementations >  Select target board > Support SONiX SN98660 Platform'"
 
 #echo -e "\tselect: Device Drivers > Memory Technology Device (MTD) support > Direct char device access to MTD devices"
-
 #echo -e "\tdeselect: 'Device Drivers > SCSI device support > SCSI CDROM support'"
 
-echo -e "\tdeselect: 'Device Drivers > Network device support > Ethernet (10 or 100Mbit) '"
-echo -e "\tselect: 'Device Drivers > Network device support > Wireless LAN > Realtek 8187 and 8187B USB support'"
+#echo -e "\tdeselect: 'Device Drivers > Network device support > Ethernet (10 or 100Mbit) '"
+#echo -e "\tselect: 'Device Drivers > Network device support > Wireless LAN > Realtek 8187 and 8187B USB support'"
 
 #echo -e "\tdeselect: 'Device Drivers > Input device support > Support for memoryless force-feedback devices'"
 #echo -e "\tdeselect: 'Device Drivers > Input device support > Event interface'"
